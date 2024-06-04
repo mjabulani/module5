216cfcd44223868d14a5d002512a42edbb2d2502
mjabulani <marekzmienka@gmail.com>
2024-06-04 17:16:37 +0200

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+        // Calc area
         Rectangle rect = new Rectangle(5, 7);
         Triangle trian = new Triangle(12, 33);
         Hexagon hex = new Hexagon(11.32);
@@ -22,17 +23,27 @@ public class Main {
         System.out.println("Triangle area (Height: " + trian.getHeight() + ", Base: " + trian.getBase()+ "):" + trian.calculateArea());
         System.out.println("Hexagon area: (A: " + hex.getA() + "): " + hex.calculateArea());
 
-
+        // Calc diameter of square
         Square sq = new Square(5);
         System.out.println("Square diamtere: " + sq.calculateDiameter());
 
+        // Print path
         Directory d = new Directory();
         d.printDirectory("src/main/resources");
 
-//        System.out.println("Creating file..." + d.createNewFile());
+        // Create new file
+        //System.out.println("Creating file..." + d.createNewFile());
 
-
+        // Read from file
         System.out.println(d.readFromFile("src/main/resources/testowyPlik.json"));
+
+        /// Are files equal?
+        File f1 = new File("src/main/resources/Test.txt");
+        File f2 = new File("src/main/resources/a.txt");
+        System.out.println("Are files equal?: " + d.areTextFilesEqual(f1, f2));
+
+        d.invertFile(f2);
     }
+
 
 }
--- src/main/java/org/example/files/Directory.java
+++ src/main/java/org/example/files/Directory.java
@@ -1,7 +1,9 @@
 package org.example.files;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Directory {
@@ -57,16 +59,59 @@ public class Directory {
     }
 
     public String readFromFile(String fileName) {
+        String text;
         StringBuilder builder = new StringBuilder();
         try {
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            builder.append(bufferedReader.readLine());
+            text = bufferedReader.readLine();
+            while (text != null) {
+                builder.append(bufferedReader.readLine());
+                text = bufferedReader.readLine();
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         }
         return builder.toString();
+    }
+    public boolean areTextFilesEqual(File file1, File file2) {
+        return file1.equals(file2);
+    }
+
+    public boolean invertFile(File file) {
+        String text;
+        String textInverted;
+        StringBuilder textBuilder = new StringBuilder();
+        StringBuilder invertedTextBuilder = new StringBuilder();
+
+        List<String> stringListBeforeInverting = new ArrayList<>();
+        List<String> stringListAfterInverting = new ArrayList<>();
+
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            text = bufferedReader.readLine();
+            textBuilder.append(bufferedReader.readLine());
+            bufferedReader.close();
+            String textBeforeInvering = textBuilder.toString();
+            for (String word : textBeforeInvering.split(" ")) {
+                stringListBeforeInverting.add(word);
+            }
+            for (int i = stringListBeforeInverting.size(); i >= 0; i--) {
+                invertedTextBuilder.append(stringListAfterInverting);
+
+            }
+            System.out.println(invertedTextBuilder.toString());
+            return true;
+
+
+        } catch (FileNotFoundException e) {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
